51a7e3b8b6e66074aa77d0e71fd4fd95c7f55bf2
tamasg05 <tamasg05@no-reply.com>
2025-05-28 17:54:51 +0200

    Preprocessing pipeline, persisting encoders, predict pipeline

--- MLPersist.py
+++ MLPersist.py
@@ -8,9 +8,13 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.neighbors import KNeighborsClassifier
 import os
+from typing import Optional
 
 class MLPersist:
     MODEL_FOLDER = "artifacts/"
+    LABEL_ENCODER = "label_encoder_dict.pkl"
+    KNN_MODEL = "knn_classifier.pkl"
+
     def __init__(self):
         if os.path.exists(MLPersist.MODEL_FOLDER):
             print(MLPersist.MODEL_FOLDER + " exists.")
@@ -41,7 +45,7 @@ class MLPersist:
 
         return df
 
-    def transfrom_data(self, df: pd.DataFrame) -> pd.DataFrame:
+    def transfrom_data(self, df: pd.DataFrame, saveEncoders=True) -> pd.DataFrame:
         # encoding the "Deck" column
         col = 'deck'
         transformed_as_df = pd.get_dummies(df[col])
@@ -51,15 +55,26 @@ class MLPersist:
         df = pd.concat([df, transformed_as_df], axis=1)
 
         # encoding adult_male and alone columns    
-        label_encoders = {}
-        for col in ["adult_male", "alone"]:
-            label_encoders[col] = LabelEncoder()
-            df[col] = label_encoders[col].fit_transform(df[col])
+        if saveEncoders: 
+            label_encoders = {}
+            for col in ["adult_male", "alone"]:
+                label_encoders[col] = LabelEncoder()
+                df[col] = label_encoders[col].fit_transform(df[col])
+            
+            self.save_artifact(label_encoders, MLPersist.MODEL_FOLDER + MLPersist.LABEL_ENCODER)
+        else:
+            label_encoders = self.load_artifact(MLPersist.MODEL_FOLDER + MLPersist.LABEL_ENCODER)
+            for col in ["adult_male", "alone"]:
+                df[col] = label_encoders[col].transform(df[col])
 
         return df
 
-    def select_features(self, df: pd.DataFrame) -> pd.DataFrame:
-        features = ['pclass', 'adult_male', 'alone', 'fare', 'deck_A', 'deck_B', 'deck_C', 'deck_D', 'deck_E', 'deck_F', 'deck_G', 'deck_None', 'survived']
+    def select_features(self, df: pd.DataFrame, full=True) -> pd.DataFrame:
+        if full:
+            features = ['pclass', 'adult_male', 'alone', 'fare', 'deck_A', 'deck_B', 'deck_C', 'deck_D', 'deck_E', 'deck_F', 'deck_G', 'deck_None', 'survived']
+        else:
+            features = ['pclass', 'adult_male', 'alone', 'fare', 'deck_A', 'deck_B', 'deck_C', 'deck_D', 'deck_E', 'deck_F', 'deck_G', 'deck_None']
+            
         df = df[features]
         return df
 
@@ -70,7 +85,7 @@ class MLPersist:
         df[col] = min_max_scaler.fit_transform(df[[col]])
         return df
     
-    def train_pipeline(self) -> None:
+    def train_pipeline(self) -> pd.DataFrame:
         file_url = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv'
         df=pd.read_csv(file_url)
 
@@ -91,10 +106,45 @@ class MLPersist:
         knn = KNeighborsClassifier(n_neighbors=neighbours)
         knn.fit(X_train, y_train)
 
-        self.save_artifact(knn, MLPersist.MODEL_FOLDER + "knn_classifier.pkl")
+        self.save_artifact(knn, MLPersist.MODEL_FOLDER + MLPersist.KNN_MODEL)
 
         y_train_pred = knn.predict(X_train)
         y_test_pred = knn.predict(X_test)
+
+        train_accuracy = accuracy_score(y_train, y_train_pred)
+        test_accuracy = accuracy_score(y_test, y_test_pred)
+        print(f"Train accuracy: {train_accuracy}, Test Accuracy: {test_accuracy}")
+        return df
+
+    def preprocess_pipeline(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = self.cleaning_dataframe(df)
+        df = self.transfrom_data(df, saveEncoders=False)
+        df = self.select_features(df, full=False)
+        df = self.scale_features(df)
+
+        return df
+
+
+    def predict(self, df: pd.DataFrame) -> Optional[pd.DataFrame]:
+        knn_path = MLPersist.MODEL_FOLDER + MLPersist.KNN_MODEL
+        
+        if not os.path.exists(knn_path):
+            print(f"No trained model is available at {knn_path}")
+            return None
+        
+        knn = self.load_artifact(MLPersist.MODEL_FOLDER + MLPersist.KNN_MODEL)
+        y = knn.predict(df)
+        
+        return y
+
+    def test_predict(self, df: pd.DataFrame) -> None:
+        y = df["survived"]
+        X = df.drop(columns="survived")
+        # random state is set for the reproducibility's sake 
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+        y_train_pred = persist.predict(X_train)
+        y_test_pred = persist.predict(X_test)
 
         train_accuracy = accuracy_score(y_train, y_train_pred)
         test_accuracy = accuracy_score(y_test, y_test_pred)
@@ -104,4 +154,12 @@ class MLPersist:
 if __name__ == "__main__":
     persist = MLPersist()
 
-    persist.train_pipeline()
+    df = persist.train_pipeline()
+    persist.test_predict(df)
+
+    file_url = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv'
+    df=pd.read_csv(file_url)
+    df = df.drop(columns="survived")
+    df = persist.preprocess_pipeline(df)
+    print(persist.predict(df))
+
